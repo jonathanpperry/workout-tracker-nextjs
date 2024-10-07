1b780536f3fdb69586ac748d7a9e548ded10b45a
Jonathan Perry <jpper@umich.edu>
2024-10-07 17:40:33 -0500

    Implement a MUI navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,42 +1,54 @@
-// components/Navbar.jsx or components/Navbar.tsx
-import React from "react";
-import styles from "./Navbar.module.css";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import { signIn, useSession, signOut } from "next-auth/react";
+import Link from "next/link";
 
-const Navbar = ({}) => {
+const Navbar = () => {
   const { data: session } = useSession();
 
   return (
-    <nav className={styles.container}>
-      <ul style={{ flex: 1, flexDirection: "row" }}>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-      </ul>
-      <div>
-        {session ? (
-          <div className={styles.userProfile}>
-            <img
-              src={session.user.image}
-              alt="Profile Picture"
-              width={50}
-              height={50}
-            />
-            <button onClick={() => signOut()}>Sign Out</button>
-          </div>
-        ) : (
-          <button onClick={() => signIn("facebook")}>
-            Sign in with Facebook
-          </button>
-        )}
-      </div>
-    </nav>
+    <AppBar position="static">
+      <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
+        <div style={{ display: "flex" }}>
+          <Link href="/" passHref>
+            <Typography
+              variant="h6"
+              sx={{
+                flexGrow: 1,
+                marginRight: "24px",
+                color: "white",
+                textDecoration: "none",
+              }}
+              component="span"
+            >
+              My Workout App
+            </Typography>
+          </Link>
+          <Button color="inherit" href="/workouts">
+            Workouts
+          </Button>
+        </div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          {session ? (
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <img
+                src={session.user.image}
+                alt="Profile Picture"
+                width={40}
+                height={40}
+                style={{ borderRadius: "50%", marginRight: "8px" }}
+              />
+              <Button color="inherit" onClick={() => signOut()}>
+                Sign Out
+              </Button>
+            </div>
+          ) : (
+            <Button color="inherit" onClick={() => signIn("facebook")}>
+              Sign in with Facebook
+            </Button>
+          )}
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 };
 
